f42bbaf70ee458b588a1b11af78c9c294fb05cc0
Rik van Noord <riknoord@hotmail.com>
2015-04-20 23:17:03 +0200

    Changed Timeline to check last item

--- app/Lib/TimelineHistory/HistoryRepository.php
+++ app/Lib/TimelineHistory/HistoryRepository.php
@@ -1,6 +1,7 @@
 <?php namespace App\Lib\TimelineHistory;
 
 use App\Models\UserProfile;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
 
 class HistoryRepository {
@@ -8,9 +9,11 @@ class HistoryRepository {
     private $HistoryListKey = "Historys";
 
     private $userprofiles = [];
+    private $authuser;
 
     public function init(){
         $this->userprofiles = Session::get($this->HistoryListKey,[]);
+        $this->authuser = Auth::User();
     }
 
     private function updatesession(){
@@ -18,6 +21,8 @@ class HistoryRepository {
     }
 
     public function add(UserProfile $userprofile){
+        if($this->LastProfileIsMine($userprofile)) return $this;
+
         array_push($this->userprofiles,$userprofile);
         $this->updatesession();
         return $this;
@@ -38,5 +43,14 @@ class HistoryRepository {
 
     public function all(){
         return array_slice($this->userprofiles,0,8);
+    }
+
+    private function LastProfileIsMine(UserProfile $userProfile){
+        if($userProfile->id == $this->authuser->id)
+            return true;
+        if(end($this->userprofiles)->id == $userProfile->id)
+            return true;
+
+        return false;
     }
 } 
